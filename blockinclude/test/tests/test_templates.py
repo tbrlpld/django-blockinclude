c8cd882c6cce194363be3eb5b3e0ca9c11d56e2b
Tibor Leupold <tibor@lpld.io>
2026-03-08 21:18:11 -0700

    Use more specific test name

--- blockinclude/test/tests/test_templates.py
+++ blockinclude/test/tests/test_templates.py
@@ -6,10 +6,10 @@ import django.test
 
 
 class TestTemplates(django.test.SimpleTestCase):
-    def test_blockinclude_passes_content_from_parent(self) -> None:
+    def test_blockinclude_passes_text_content_from_parent(self) -> None:
         response = django.shortcuts.render(
             request=None,
-            template_name="tests/test-blockinclude-passes-content-from-parent.html",
+            template_name="tests/test-blockinclude-passes-text-content-from-parent.html",
             context={},
         )
 
