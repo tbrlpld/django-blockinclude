5d7aca0b6e4bd52aeada10cd4a8c151f58ea7658
Tibor Leupold <tibor@lpld.io>
2026-03-12 21:37:50 -0700

    Test slot surrounded by blockinclude content

--- blockinclude/test/tests/test_templates.py
+++ blockinclude/test/tests/test_templates.py
@@ -192,3 +192,15 @@ class TestTemplates(django.test.SimpleTestCase):
         self.assertStringInTag(string="Phasellus", tag=the_box.header)
         assert isinstance(the_box.footer, bs4.Tag)
         self.assertStringInTag(string="Minima", tag=the_box.footer)
+
+    def test_slot_surrounded_by_blockinclude_content(self) -> None:
+        soup = self.get_soup_for_template(
+            template_name="tests/test-12-slot-surrounded-by-blockinclude-content.html",
+        )
+
+        the_box = self.get_included_box(soup=soup)
+        assert isinstance(the_box.div, bs4.Tag)
+        self.assertStringInTag(string="Lorem", tag=the_box.div)
+        self.assertStringInTag(string="Minima", tag=the_box.div)
+        assert isinstance(the_box.header, bs4.Tag)
+        self.assertStringInTag(string="Phasellus", tag=the_box.header)
